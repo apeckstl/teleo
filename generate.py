b6b5bb8d9842ef6c5396309738a14351027cee52
avery <averypeckstl@gmail.com>
2019-01-25 20:53:49 -0600

    better user interaction. Refactored. Needs font size/positioning tweaking, and less awkward prompts

--- generate.py
+++ generate.py
@@ -8,6 +8,8 @@ import sys
 TITLE_LAYOUT = 0
 BLANK = 10
 FONT = "Calibri"
+FILE_LOC = "../"
+SERMON_SERIES = ""
 
 class LiturgyElem:
     def __init__(self,f_name):
@@ -23,89 +25,101 @@ class LiturgyElem:
             self.title = lyrics[0]
             self.lyrics = lyrics[1:]
 
+def parseFileLines(filename):
+    with open(filename,"r") as file:
+        return file.readlines()
+
+def parseFile(filename):
+    with open(filename,"r") as file:
+        return file.read()
+
+
 #creates a textbox for a song slide, with parameters for placement, size, and boldness
 #returns a paragraph!!
-def createVerseTextBox(verse_slide,dim,size,bold):
-        shapes = verse_slide.shapes
-        verse_text = shapes.add_textbox(Inches(dim[0]),Inches(dim[1]),Inches(dim[2]),Inches(dim[3]))
-        frame = verse_text.text_frame
-        frame.word_wrap = True
-        p = frame.add_paragraph()
-        p.alignment = PP_ALIGN.CENTER
-        p.font.name = 'Calibri'
-        p.font.size = Pt(size)
-        p.font.bold = bold
-        p.font.color.rgb = RGBColor(255,255,255)
-        return p
-
-#method done. Adds a song to the presentation at the end, pulling from a Song object
-def addSong(song,prs):
-    #add title slide for song
-    song_title_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-    title = createVerseTextBox(song_title_slide,(0.375,0.25,9.25,1.25),36,True)
-    title.text = song.title
-    #put first verse of song on title slide
-    p = createVerseTextBox(song_title_slide,(0.5,1.5,9,6),30,False)
-    p.text = song.lyrics[0]
-    #loop through remaining verses and add slides
-    for verse in song.lyrics[1:]:
-        verse_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-        p = createVerseTextBox(verse_slide,(0.5,1.5,9,6),30,False)
-        p.text = verse
-
-#Done, creates text frame on a slide with given dimensions and adds 2 paragraphs
-def createTextBox(slide,dim):
+def addTextFrame(slide,dim):
         shapes = slide.shapes
         verse_text = shapes.add_textbox(Inches(dim[0]),Inches(dim[1]),Inches(dim[2]),Inches(dim[3]))
         frame = verse_text.text_frame
         frame.word_wrap = True
-        p = frame.paragraphs[0]
-        p.alignment = PP_ALIGN.CENTER
-        p.font.name = 'Calibri'
-        p.font.color.rgb = RGBColor(255,255,255)
-        p = frame.add_paragraph()
-        p.alignment = PP_ALIGN.CENTER
-        p.font.name = 'Calibri'
-        p.font.color.rgb = RGBColor(255,255,255)
-        p = frame.add_paragraph()
-        p.alignment = PP_ALIGN.CENTER
-        p.font.name = 'Calibri'
-        p.font.color.rgb = RGBColor(255,255,255)
         return frame
 
-def addCallConfSlide(ref,prs,flag):
-    #add title slide for service
-    first_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-    heading = createTextBox(first_slide,(0.5,0.25,9.5,7.25))
-    head = ref.title.split('\n')
-    title = heading.paragraphs[0]
-    title.font.size = Pt(32)
-    title.font.bold = True
-    title.text = head[0]
-    i = 1
-    if flag:
-        i = 2
-        mid = heading.paragraphs[1]
-        mid.font.size = Pt(24)
-        mid.text = "Now hear the gracious word of God from:"
-    sub = heading.paragraphs[i]
-    sub.font.size = Pt(30)
-    sub.font.bold = True
-    sub.text = head[1]
-    #put first verse of song on title slide
-    p = createVerseTextBox(first_slide,(0.5,1.5,9,6),28,False)
+#adds a center justified paragraph with given font size and boldness to the given frame
+def addCenterParagraph(frame,size,bold):
+    if (frame.paragraphs[0].font.name == 'Calibri'):
+        p = frame.add_paragraph()
+    else:
+        p = frame.paragraphs[0]
+    p.alignment = PP_ALIGN.CENTER
+    p.font.name = 'Calibri'
+    p.font.size = Pt(size)
+    p.font.bold = bold
+    p.font.color.rgb = RGBColor(255,255,255)
+    return p
+
+#adds a left justified paragraph with given font size and boldness to the given frame
+def addLeftParagraph(frame,size,bold):
+    print(frame.paragraphs[0].font.name)
+    if (frame.paragraphs[0].font.name == 'Calibri'):
+        p = frame.add_paragraph()
+    else:
+        p = frame.paragraphs[0]
     p.alignment = PP_ALIGN.LEFT
-    p.text = ref.lyrics[0]
+    p.font.name = 'Calibri'
+    p.font.size = Pt(size)
+    p.font.bold = bold
+    p.font.color.rgb = RGBColor(255,255,255)
+    return frame
+
+#adds a song to the presentation at the end, pulling from a LiturgyElem object
+def addSong(prs,filename):
+    #add title slide for song
+    song = LiturgyElem(filename)
+    song.parseLiturgyFile("\n\n")
+    song_title_slide = addBlank(prs)
+    title = addTextFrame(song_title_slide,(0.375,0.25,9.25,1.25))
+    p = addCenterParagraph(title,36,True)
+    title.text = song.title
+    #put first verse of song on title slide
+    frame = addTextFrame(song_title_slide,(0.5,1.5,9,6))
+    p = addCenterParagraph(frame,30,False)
+    p.text = song.lyrics[0]
     #loop through remaining verses and add slides
-    for verse in ref.lyrics[1:]:
+    for verse in song.lyrics[1:]:
         verse_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-        p = createVerseTextBox(verse_slide,(0.5,1,9,6.5),28,False)
-        p.alignment = PP_ALIGN.LEFT
+        frame = addTextFrame(verse_slide,(0.5,1.5,9,6))
+        p = addCenterParagraph(frame,30,False)
         p.text = verse
 
 
+def addResponsiveSlide(prs,grace,segment):
+    #add title slide for segment
+    first_slide = addBlank(prs)
+    frame = addTextFrame(first_slide,(0.5,0.25,9,7.25))
+    title = addCenterParagraph(frame,30,True)
+    #assurance,confession,call
+    title.text = segment
+    if grace:
+        mid = addCenterParagraph(frame,24,False)
+        mid.italic = True
+        mid.text = "Now hear the gracious word of God from:"
+    sub = addCenterParagraph(frame,30,True)
+    sub.text = input("What's the reference for the slide? ")
+    filename = FILE_LOC + input("Filename? ")
+    sections = parseFile(filename).split("$$$")
+    #put first section of response
+    frame = addTextFrame(first_slide,(0.5,1.5,9,6))
+    p = addLeftParagraph(frame,28,False)
+    p.text = sections[0]
+    #loop through remaining sections and add slides
+    for section in sections[1:]:
+        slide = addBlank(prs)
+        frame = addTextFrame(slide,(0.5,0.75,9,6.75))
+        p = addLeftParagraph(frame,28,False)
+        p.text = section
+
+
 def addBlank(prs):
-    prs.slides.add_slide(prs.slide_layouts[BLANK])
+    return prs.slides.add_slide(prs.slide_layouts[BLANK])
 
 #open template
 prs = Presentation("template.pptx")
@@ -118,94 +132,82 @@ title_placeholder.text = "Welcome to Living Hope Church!"
 addBlank(prs)
 
 #first song
-song = LiturgyElem(sys.argv[1])
-song.parseLiturgyFile("\n\n")
-addSong(song,prs)
+filename = FILE_LOC + input("Name of first song? ")
+addSong(prs,filename)
 addBlank(prs)
 
 #call to worship
-call = LiturgyElem(sys.argv[2])
-call.parseLiturgyFile("$$$\n")
-addCallConfSlide(call,prs,False)
+addResponsiveSlide(prs,False,"Call to Worship")
 addBlank(prs)
 
 #second, third, fourth song
-for i in range(3,6):
-    print(sys.argv[i])
-    song = LiturgyElem(sys.argv[i])
-    song.parseLiturgyFile("\n\n")
-    addSong(song,prs)
+for i in range(3):
+    filename = FILE_LOC + input("Name of song? ")
+    addSong(prs,filename)
     addBlank(prs)
 
-conf = LiturgyElem(sys.argv[6])
-conf.parseLiturgyFile("$$$\n")
-addCallConfSlide(conf,prs,False)
+#confession
+addResponsiveSlide(prs,False,"Confession of Sin")
 addBlank(prs)
 
 #assurance of grace
-grace = LiturgyElem(sys.argv[7])
-grace.parseLiturgyFile("$$$\n")
-addCallConfSlide(grace,prs,True)
+addResponsiveSlide(prs,False,"Assurance of Grace")
 addBlank(prs)
 
 #offering
-offering_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-heading = createVerseTextBox(offering_slide,(0,0.5,10,1.5),32,True)
-heading.text = "Worship through Tithes and Offerings"
-p = createVerseTextBox(offering_slide,(0,1.5,10,6),28,False)
-with open(sys.argv[8],"r") as file:
-    offering = file.read()
-p.text = offering
+offering_slide = addBlank(prs)
+frame = addTextFrame(offering_slide,(0,0.5,10,1.5))
+p = addCenterParagraph(frame,32,True)
+p.text = "Worship through Tithes and Offerings"
+frame = addTextFrame(offering_slide,(0,1.5,10,6))
+p = addCenterParagraph(frame,28,False)
+p.text = parseFile(FILE_LOC + "Offering.txt")
 addBlank(prs)
 
 #offering song
-song = LiturgyElem(sys.argv[9])
-song.parseLiturgyFile("\n\n")
-addSong(song,prs)
+filename = FILE_LOC + input("Name of song? ")
+addSong(prs,filename)
 addBlank(prs)
 
 #Prayers of the people
-prayer_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-text = createVerseTextBox(prayer_slide,(0,1.5,10,1.5),30,True)
-text.text = "Prayers of the People"
+prayer_slide = addBlank(prs)
+frame = addTextFrame(prayer_slide,(0,1.5,10,1.5))
+p = addCenterParagraph(frame,30,True)
+if (input("Is it prayers of the people this week? ") == "y"):
+    p.text = "Prayers of the People"
+else:
+    p.text = "Prayers for our Church, Community, and World"
 addBlank(prs)
 
 #Announcements
-ann_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-text = createVerseTextBox(ann_slide,(0.25,0,10,7.25),28,True)
-text.text = "Announcements"
+ann_slide = addBlank(prs)
+frame = addTextFrame(ann_slide,(0.25,0,9,7))
+p = addCenterParagraph(frame,28,True)
+p.text = "Announcements"
 addBlank(prs)
 
+#Sermon
 sermon_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-heading = createTextBox(sermon_slide,(0.75,1.5,8.5,4.5))
-with open( sys.argv[10],"r") as file:
-    name = file.read()
-title_parts = name.split("\n")
-first_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-title = heading.paragraphs[0]
-title.font.size = Pt(30)
-title.font.bold = True
-title.text = title_parts[0]
-mid = heading.paragraphs[1]
-mid.font.size = Pt(24)
-mid.font.italic = True
-mid.font.bold = True
-mid.text = title_parts[1]
-sub = heading.paragraphs[2]
-sub.font.size = Pt(30)
-sub.font.bold = True
-sub.text = title_parts[2]
+frame = addTextFrame(sermon_slide,(0.75,1.5,8.5,4.5))
+title = addCenterParagraph(frame,30,True)
+title.text = input("Sermon title? ")
+mid = addCenterParagraph(frame,24,True)
+mid.font.input = True
+mid.text = SERMON_SERIES
+sub = addCenterParagraph(frame,30,True)
+sub.text = input("Sermon reference? ")
 
 #closing song
-song = LiturgyElem(sys.argv[11])
-song.parseLiturgyFile("\n\n")
-addSong(song,prs)
+filename = FILE_LOC + input("Name of song? ")
+addSong(prs,filename)
 addBlank(prs)
 
 bene_slide = prs.slides.add_slide(prs.slide_layouts[BLANK])
-text = createVerseTextBox(bene_slide,(0,0.5,10,1),44,True)
-text.text = "Benediction"
-rest = createVerseTextBox(bene_slide,(0,1.75,10,3.5),40,False)
-rest.text = "A blessing from God’s Word\nas we go out into God’s world."
+frame = addTextFrame(bene_slide,(0,0.5,10,1))
+p = addCenterParagraph(frame,44,True)
+p.text = "Benediction"
+rest = addTextFrame(bene_slide,(0,1.75,10,3.5))
+p = addCenterParagraph(rest,40,False)
+p.text = "A blessing from God’s Word\nas we go out into God’s world."
 
 prs.save("slidesbase.pptx")
